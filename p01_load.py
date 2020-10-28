2f551c3cb189e784767427df5cb1c53454949ee4
Antonio LaTorre <atorre@fi.upm.es>
2020-10-28 17:30:00 +0100

    Remove unneeded files.

--- p01_load.py
+++
@@ -1,9 +0,0 @@
-def load_data(file_locations, sqlcontext):
-    print("Loading CSV files to sqlcontext...")
-    police_data_df = sqlcontext.read.format("csv")\
-                .option("header", "true")\
-                .option("inferSchema", "true")\
-                .load(file_locations)
-    print('Load Complete')
-    return police_data_df
-                
